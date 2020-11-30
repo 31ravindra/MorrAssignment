638a11e7a1855910bfd61200d70ba7ecbd6a86e4
Ravindra Patidar <ravindra.patidar@oracle.com>
2020-11-30 16:18:30 +0530

    add all text field

--- MorrAssignment/MorrAssignment/Constants/Constants.swift
+++ MorrAssignment/MorrAssignment/Constants/Constants.swift
@@ -16,3 +16,26 @@ struct Constants {
 struct Colors {
     static let blueHex = "#3c4faf"
 }
+
+
+enum CardType: String {
+    case Unknown, Amex, Visa, MasterCard, Discover
+    
+    static let allCards = [Amex, Visa, MasterCard, Discover]
+    
+    var regex : String {
+        switch self {
+        case .Amex:
+            return "^3[47][0-9]{5,}$"
+        case .Visa:
+            return "^4[0-9]{6,}([0-9]{3})?$"
+        case .MasterCard:
+            return "^(5[1-5][0-9]{4}|677189)[0-9]{5,}$"
+        case .Discover:
+            return "^6(?:011|5[0-9]{2})[0-9]{3,}$"
+        default:
+            return ""
+        }
+    }
+}
+
--- MorrAssignment/MorrAssignment/ViewControllers/CreditCardInputViewController.swift
+++ MorrAssignment/MorrAssignment/ViewControllers/CreditCardInputViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class CreditCardInputViewController: UIViewController {
     
+   
+    @IBOutlet weak var customCardNumberTxtField: CustomOutlinedTxtField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+       // customCardNumberTxtField.layer.borderWidth = 2.0
         
         
        
@@ -27,5 +34,80 @@ class CreditCardInputViewController: UIViewController {
         
     }
     
+    func checkCardNumber(input: String) -> (type: CardType, formatted: String, valid: Bool) {
+        // Get only numbers from the input string
+      //  let numberOnly = input.stringByReplacingOccurrencesOfString("[^0-9]", withString: "", options: .RegularExpressionSearch)
+        
+        let numberOnly = input.replacingOccurrences(of: "[^0-9]", with: "")
+        
+        var type: CardType = .Unknown
+        var formatted = ""
+        var valid = false
+        
+        // detect card type
+        for card in CardType.allCards {
+            if (matchesRegex(regex: card.regex, text: numberOnly)) {
+                type = card
+                break
+            }
+        }
+        
+        // check validity
+        valid = luhnCheck(number: numberOnly)
+        
+        // format
+        var formatted4 = ""
+        for character in numberOnly{
+            if formatted4.count == 4 {
+                formatted += formatted4 + " "
+                formatted4 = ""
+            }
+            formatted4.append(character)
+        }
+        
+        formatted += formatted4 // the rest
+        
+        // return the tuple
+        return (type, formatted, valid)
+    }
+
+    
+    
+    func matchesRegex(regex: String!, text: String!) -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: regex, options: [.caseInsensitive])
+            let nsString = text as NSString
+            let match = regex.firstMatch(in: text, options: [], range: NSMakeRange(0, nsString.length))
+            return (match != nil)
+        } catch {
+            return false
+        }
+    }
+
+    func luhnCheck(number: String) -> Bool {
+        var sum = 0
+        let digitStrings = number.reversed()
+        
+        for (index, char) in digitStrings.enumerated() {
+            guard let digit = Int(String(char)) else { return false }
+            let odd = index % 2 == 1
+            
+            switch (odd, digit) {
+            case (true, 9):
+                sum += 9
+            case (true, 0...8):
+                sum += (digit * 2) % 9
+            default:
+                sum += digit
+            }
+        }
+        
+        return sum % 10 == 0
+    }
+
+    
+    @IBAction func btnSubmitClick(_ sender: Any) {
+        let text = customCardNumberTxtField.textField.text
+    }
     
 }
